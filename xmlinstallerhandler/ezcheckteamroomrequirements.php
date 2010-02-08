f7b9f5ea27e8b6d4afd154bbb365e1a87e544659
Paul Borgermans <pb@ez.no>
2010-02-08 20:49:27 +0000

    - adding version compliancy with ezp 4.3-alpha1 build
    git-svn-id: http://svn.projects.ez.no/ezteamroom/trunk@6 5de17773-5fd0-4f67-98cd-37ccb56b0513

--- xmlinstallerhandler/ezcheckteamroomrequirements.php
+++ xmlinstallerhandler/ezcheckteamroomrequirements.php
@@ -56,7 +56,7 @@ class eZCheckTeamroomRequirements extends eZXMLInstallerHandler
         }
         else
         {
-            $validVersions = array( '4.0.0', '4.0.1', '4.0.2', '4.0.3', '4.0.4', '4.0.5', '4.0.6', '4.0.7', '4.1.0', '4.1.1', '4.1.2', '4.1.3', '4.1.4', '4.2.0' );
+            $validVersions = array( '4.0.0', '4.0.1', '4.0.2', '4.0.3', '4.0.4', '4.0.5', '4.0.6', '4.0.7', '4.1.0', '4.1.1', '4.1.2', '4.1.3', '4.1.4', '4.2.0', '4.3.0-alpha1' );
             $version = substr( $dbOK[0]['value'], 0, 5 );
             if ( !in_array( $version, $validVersions ) )
             {
