a58ab1673032de1fba477ccecdbd47363e6fa65b
ms@b055900d-e7fb-4479-9051-199c62356f13 <ms@b055900d-e7fb-4479-9051-199c62356f13>
2012-03-02 09:37:30 +0000

    standard settings wieder hergestellt
    git-svn-id: http://svn.ezservice.de/svn/circit/Extensions/ezteamroom/trunk@142 b055900d-e7fb-4479-9051-199c62356f13

--- extension/ezteamroom/settings/siteaccess/ezteamroom/site.ini.append.php
+++ extension/ezteamroom/settings/siteaccess/ezteamroom/site.ini.append.php
@@ -11,7 +11,7 @@ CachedViewModes=full;sitemap;pdf;module_widget;module_widget_latest;manage;teamr
 CachedViewPreferences[]
 CachedViewPreferences[full]=teamroom_files_list_limit=10;teamroom_folder_list_limit=10;teamroom_milestone_list_limit=10;teamroom_list_limit=10;teamroom_forum_list_limit=10;personalfrontpage_widgetlist_1441=0;personalfrontpage_displaydescription=1;teamroom_blog_list_limit=10;teamroom_calendar_limit=10;teamroom_documents_list_limit=10;teamroom_tasklist_list_limit=10;teamroom_list_limit;teamroom_member_list_limit=10
 
-#[SiteAccessSettings]
-#PathPrefix=Teamrooms
+[SiteAccessSettings]
+PathPrefix=Teamrooms
 
 */ ?>
